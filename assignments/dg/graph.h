e40c324300bcf9bae7f51c55ae3b2021c86d965e
Clive <clive41@gmail.com>
2019-07-24 18:28:10 +1000

    Initalised graph structure and attempted first graph constructor inline (out of line has bugs?)

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -7,61 +7,59 @@
 
 namespace gdwg {
 
-template <typename N, typename E>
-class Graph {
- public:
-  class const_iterator {};
-  Graph<N, E>();
-  gdwg::Graph<N, E>(std::vector<N>::const_iterator, std::vector<N>::const_iterator);
-  bool InsertNode(const N&);
+  template <typename N, typename E>
+  class Graph {
+    public:
+      class const_iterator {};
 
- private:
+      // Vector Constructor
+      Graph<N, E>(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2) {
+        for (auto it = c1; it != c2; it++) {
+          // Check for duplicates in initaliser vector
+          bool alreadyInList = false;
+          for (const auto& item : nodeList_) {
+            if (*item.getValue() == *it) {
+              alreadyInList = true;
+              break;
+            }
+          }
+          if (!alreadyInList) {
+            nodeList_.push(std::shared_ptr<Node>(new Node(*it)));
+          }
+        }
+      };
 
-  class Node {
-   public:
-    explicit Node(N value) {
-      this->value_ = value;
-      this->deg_in_ = 0;
-      this->deg_out_ = 0;
+    private:
+      class Node {
+        N value_;
+        std::vector<Node*> parents_;
+        std::vector<Node*> children_;
+
+        Node(N value) : value_(value) {}
+        inline std::vector<Node*> getChildren() {return children_;}
+        inline std::vector<Node*> getParents() {return parents_;}
+        inline N getValue() {return value_;}
+      };
+      class Edge {
+       private:
+        Node* start_;
+        Node* end_;
+        E weight_;
+       public:
+        Edge(N start, N end, E weight) {
+          start_(std::weak_ptr<Node>(new Node(start))), end_(std::weak_ptr<Node>(new Node(end))), weight_(weight);
+        }
+      };
+      std::vector<std::shared_ptr<Node>> nodeList_;
+      std::vector<std::shared_ptr<Edge>> edgeList_;
     };
-    std::map<N&, E> getChildren() {return children_;};
-    bool addEdge(const N&, const E&);
-
-   private:
-    N value_;
-    int deg_in_;
-    int deg_out_;
-    std::map<N&,E> parents_;
-    std::map<N&,E> children_;
-  };
-
-  bool addEdge(const N& dest, const E& weight) {
-    this->deg_out++;
-    this->children_.push(dest,weight);
-  }
-
-  std::vector<std::unique_ptr<Node>> nodeList_;
-
-};
-
-template<typename N, typename E>
-gdwg::Graph<N, E>() {
-
-}
-
-template<typename N, typename E>
-bool gdwg::InsertNode(const N& val) {
-  if (this->checkExists(val)) {
-    return false;
-  }
-  std::unique_ptr<Node> newNode = std::make_unique<Node> (val);
-  this->nodeList_.push(newNode);
-  return true;
-}
-
 
 }  // namespace gdwg
 
+
 #include "assignments/dg/graph.tpp"
+
+//template<typename N, typename E>
+//gdwg::Graph<N, E>(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2) {  }
 
 #endif  // ASSIGNMENTS_DG_GRAPH_H_

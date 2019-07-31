04a259b47e615bcd1b7b442c0f69726767da3524
vaishnavi-bapat <vaishnavi.bapat@gmail.com>
2019-08-01 07:56:19 +1000

    const_iterator operators

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -5,34 +5,33 @@
 #include <map>
 #include <memory>
 
+class Node;
+class Edge;
+
 namespace gdwg {
 
     template <typename N, typename E>
     class Graph {
     public:
         class const_iterator {
+        public:
             using iterator_category = std::bidirectional_iterator_tag;
             using value_type = std::tuple<N, N, E>;
             using reference = std::tuple<const N&, const N&, const E&>;
             using pointer = std::tuple<N*, N*, E*>;
             using difference_type = int;
 
+            // This one isn't strictly required, but it's nice to have.
+            pointer operator->() const { return &(operator*()); }
             reference operator*() const;
-            const_iterator& operator++() {
-                // perform operation
-                return *this;
-            }
-            const_iterator operator++(int) {
+            const_iterator& operator++();
+            const const_iterator operator++(int) {
                 auto copy{*this};
                 ++(*this);
                 return copy;
             }
-
-            const_iterator& operator--() {
-                // perform operation
-                return *this;
-            }
-            const_iterator operator--(int) {
+            const_iterator& operator--();
+            const const_iterator operator--(int) {
                 auto copy{*this};
                 --(*this);
                 return copy;
@@ -40,7 +39,9 @@ namespace gdwg {
 
             friend bool operator==(const const_iterator& lhs,
                     const const_iterator rhs) {
-//                return it1.getValue() == rhs.outer_ && (lhs.outer_ == lhs.sentinel_ || lhs.inner_ == rhs.inner_);
+                return (lhs.node_iter_ == rhs.node_iter_)
+                       && (lhs.edge_iter_ == rhs.edge_iter_)
+                       && (lhs.weight_iter_ == rhs.weight_iter_);
             }
 
             friend bool operator!=(const const_iterator& lhs,
@@ -49,15 +50,30 @@ namespace gdwg {
             }
 
         private:
-            std::vector<std::string>::iterator outer_;
-            const std::vector<std::string>::iterator sentinel_;
-            std::string::iterator inner_;
-
             friend class Graph;
-//            iterator(const decltype(outer_)& outer, const decltype(sentinel_)& sentinel, const decltype(inner_)& inner): outer_{outer}, sentinel_{sentinel}, inner_{inner} {}
+
+            std::vector<std::shared_ptr<Node>>::const_iterator node_iter_;
+            const std::vector<std::shared_ptr<Node>>::const_iterator node_sentinel_;
+            typename std::map<N, std::vector<E>>::const_iterator edge_iter_;
+            typename std::vector<E>::const_iterator weight_iter_;
+
+            const_iterator(const decltype(node_iter_)& node_iter,
+                    const decltype (node_sentinel_)& node_sentinel,
+                    const decltype (edge_iter_)& edge_iter,
+                    const decltype (weight_iter_) weight_iter):
+                    node_iter_{node_iter},
+                    node_sentinel_{node_sentinel},
+                    edge_iter_{edge_iter},
+                    weight_iter_{weight_iter} {}
         };
 
+        const_iterator begin();
+        const_iterator end();
+        const_iterator rbegin();
+        const_iterator rend();
+
         // Vector Constructor
+        Graph<N, E>();
         Graph<N, E>(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2);
         Graph<N, E>(typename std::vector<std::tuple<N, N, E>>::const_iterator, typename std::vector<std::tuple<N, N, E>>::const_iterator);
 
@@ -65,18 +81,23 @@ namespace gdwg {
         bool InsertNode(const N&);
         bool DeleteNode(const N&);
 
-
     private:
         class Node {
+        private:
             N value_;
-            std::vector<Node*> parents_;
-            std::vector<Node*> children_;
+//            std::vector<Node*> parents_;
+            std::vector<std::weak_ptr<Node>> children_;
+            std::map<std::weak_ptr<N>, std::vector<E>> edges_;
 
+        public:
+            Node();
             Node(N value) : value_(value) {}
-            inline std::vector<Node*> getChildren() {return children_;}
-            inline std::vector<Node*> getParents() {return parents_;}
+            inline std::vector<std::weak_ptr<Node>> getChildren() {return children_;}
+            inline std::map<std::weak_ptr<N>, std::vector<E>> getEdges() {return edges_;}
             inline N getValue() {return value_;}
+            bool addEdge(std::weak_ptr<Node>, const E&);
         };
+
         class Edge {
         private:
             Node* src_;
@@ -90,6 +111,7 @@ namespace gdwg {
             Node* getDst() {return dst_;}
             E getWeight() {return weight_;}
         };
+
         std::vector<std::shared_ptr<Node>> nodeList_;
         std::vector<std::shared_ptr<Edge>> edgeList_;
     };
@@ -101,5 +123,125 @@ namespace gdwg {
 
 //template<typename N, typename E>
 //gdwg::Graph<N, E>(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2) {  }
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator& gdwg::Graph<N, E>::const_iterator::operator++() {
+    ++weight_iter_;
+
+    if (weight_iter_ == *edge_iter_->end()) {
+        ++edge_iter_;
+
+        if (edge_iter_ == (*node_iter_)->end()) {
+
+            do {
+                ++node_iter_;
+            } while (node_iter_ != node_sentinel_
+                     && (*node_iter_)->begin() == (*node_iter_)->end());
+
+            if (node_iter_ != node_sentinel_) {
+                edge_iter_ = (*node_iter_)->begin();
+                weight_iter_ = edge_iter_->begin();
+            }
+        }
+    }
+}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator& gdwg::Graph<N, E>::const_iterator::operator--() {
+
+    if (weight_iter_ == edge_iter_->begin()) {
+
+        if (edge_iter_ == (*node_iter_)->begin()) {
+
+            do {
+                --node_iter_;
+            } while (node_iter_ != node_sentinel_
+                     && (*node_iter_)->begin() == (*node_iter_)->end());
+
+            if (node_iter_ != node_sentinel_) {
+                edge_iter_ = (*node_iter_)->end();
+                --edge_iter_;
+
+                weight_iter_ = edge_iter_->end();
+                --weight_iter_;
+            }
+
+        } else {
+            --edge_iter_;
+
+            weight_iter_ = edge_iter_->end();
+            --weight_iter_;
+        }
+
+    } else {
+        --weight_iter_;
+    }
+
+}
+
+//template<typename N, typename E>
+//typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::const_iterator::operator++(int) {
+//    auto copy{*this};
+//    ++(*this);
+//    return copy;
+//}
+//
+//template<typename N, typename E>
+//typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::const_iterator::operator--(int) {
+//    auto copy{*this};
+//    --(*this);
+//    return copy;
+//}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator::reference
+gdwg::Graph<N, E>::const_iterator::operator*() const {
+    return {(*node_iter_).getValue(), *edge_iter_, *weight_iter_};
+}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::begin() {
+    typename std::vector<std::shared_ptr<Node>>::iterator it = nodeList_.begin();
+
+    while (*it.getChildren().size() == 0) {
+        ++it;
+    }
+
+    if (it == nodeList_.end()) {
+        return end();
+    }
+
+    std::map<N, std::vector<E>> children = (*it).getChildren();
+    std::vector<E> weights = children[0];
+    return {it, nodeList_.end(), children.begin(), weights.begin()};
+}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::end() {
+    return {nodeList_.end(), nodeList_.end(), {}, {}};
+}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::rbegin() {
+    typename std::vector<std::shared_ptr<Node>>::const_iterator it = nodeList_
+            .rbegin();
+
+    while (*it.getChildren().size() == 0) {
+        ++it;
+    }
+
+    if (it == nodeList_.rend()) {
+        return rend();
+    }
+
+    std::map<N, std::vector<E>> children = *it.getChildren();
+    std::vector<E> weights = children[children.size()-1];
+    return {it, nodeList_.rend(), children.rbegin(), weights.rbegin()};
+}
+
+template<typename N, typename E>
+typename gdwg::Graph<N, E>::const_iterator gdwg::Graph<N, E>::rend() {
+    return {nodeList_.rend(), nodeList_.rend(), {}, {}};
+}
 
 #endif  // ASSIGNMENTS_DG_GRAPH_H_
--- assignments/dg/graph.tpp
+++ assignments/dg/graph.tpp
@@ -1,84 +1,1 @@
-
-
-#include "assignments/dg/graph.h"
-
-template<typename N, typename E>
-gdwg::Graph<N, E>::Graph(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2) {
-  for (auto it = c1; it != c2; it++) {
-    // Check for duplicates in initaliser vector
-    bool alreadyInList = false;
-    for (const auto& item : nodeList_) {
-      if (*item.getValue() == *it) {
-        alreadyInList = true;
-        break;
-      }
-    }
-    if (!alreadyInList) {
-      nodeList_.push(std::shared_ptr<Node>(new Node(*it)));
-    }
-  }
-}
-
-template<typename N, typename E>
-gdwg::Graph<N, E>::Graph(typename std::vector<std::tuple<N, N, E>>::const_iterator c1, typename std::vector<std::tuple<N, N, E>>::const_iterator c2) {
-  for (auto it = c1; it != c2; it++) {
-    // Check for duplicates in initaliser vector
-    bool alreadyInList = false;
-    for (auto i = 0; i < 2; i++) {
-      for (const auto& node : nodeList_) {
-        if (*node.getValue() == *it[i]) {
-          alreadyInList = true;
-          break;
-        }
-      }
-      if (!alreadyInList) {
-        nodeList_.push(std::shared_ptr<Node>(new Node(*it)));
-      }
-    }
-    InsertEdge(it[0], it[1], it[2]);
-  }
-}
-
-
-template<typename N, typename E>
-bool gdwg::Graph<N, E>::InsertNode(const N& n) {
-  for (const auto& node : nodeList_) {
-    if (node->getValue() == n) {
-      return false;
-    }
-  }
-  nodeList_.push(std::shared_ptr<Node>(new Node(n)));
-  return true;
-}
-
-template<typename N, typename E>
-bool gdwg::Graph<N, E>::InsertEdge(const N& src, const N& dst, const E& w) {
-  InsertNode(src);
-  InsertNode(dst);
-  std::shared_ptr<Node> srcNode;
-  std::shared_ptr<Node> dstNode;
-  for (const auto& node : nodeList_) {
-    if (node->getValue() == src) {
-      srcNode = node;
-    }
-    if (node->getValue() == dst) {
-      dstNode = node;
-    }
-  }
-  // src and dst must be inside nodeList_ because of InsertNode()
-  auto wkSrcNode = std::weak_ptr<Node>(srcNode);
-  auto wkDstNode = std::weak_ptr<Node>(dstNode);
-
-  for (const auto& edge : edgeList_) {
-    if (edge->getSrc->getValue == src && edge->getDst->getValue == dst && edge->getWeight == w) {
-      return false;
-    }
-  }
-  edgeList_.push(srcNode, dstNode, w);
-  return true;
-}
-
-//template<typename N, typename E>
-//bool gdwg::Graph<N, E>::DeleteNode(const N& n) {
-//
-//}
+#include "assignments/dg/graph.h"template<typename N, typename E>gdwg::Graph<N, E>::Graph(typename std::vector<N>::const_iterator c1, typename std::vector<N>::const_iterator c2) {    for (auto it = c1; it != c2; it++) {        // Check for duplicates in initaliser vector        bool alreadyInList = false;        for (const auto& item : nodeList_) {            if (*item.getValue() == *it) {                alreadyInList = true;                break;            }        }        if (!alreadyInList) {            nodeList_.push(std::shared_ptr<Node>(new Node(*it)));        }    }}template<typename N, typename E>gdwg::Graph<N, E>::Graph(typename std::vector<std::tuple<N, N, E>>::const_iterator c1, typename std::vector<std::tuple<N, N, E>>::const_iterator c2) {    for (auto it = c1; it != c2; it++) {        // Check for duplicates in initaliser vector        bool alreadyInList = false;        for (auto i = 0; i < 2; i++) {            for (const auto& node : nodeList_) {                if (*node.getValue() == *it[i]) {                    alreadyInList = true;                    break;                }            }            if (!alreadyInList) {                nodeList_.push(std::shared_ptr<Node>(new Node(*it)));            }        }        InsertEdge(it[0], it[1], it[2]);    }}template<typename N, typename E>bool gdwg::Graph<N, E>::InsertNode(const N& n) {    for (const auto& node : nodeList_) {        if (node->getValue() == n) {            return false;        }    }    nodeList_.push(std::shared_ptr<Node>(new Node(n)));    return true;}template<typename N, typename E>bool gdwg::Graph<N, E>::InsertEdge(const N& src, const N& dst, const E& w) {    InsertNode(src);    InsertNode(dst);    std::shared_ptr<Node> srcNode;    std::shared_ptr<Node> dstNode;    for (const auto& node : nodeList_) {        if (node->getValue() == src) {            srcNode = node;        }        if (node->getValue() == dst) {            dstNode = node;        }    }    // src and dst must be inside nodeList_ because of InsertNode()    auto wkSrcNode = std::weak_ptr<Node>(srcNode);    auto wkDstNode = std::weak_ptr<Node>(dstNode);    for (const auto& edge : edgeList_) {        if (edge->getSrc->getValue == src && edge->getDst->getValue == dst && edge->getWeight == w) {            return false;        }    }    edgeList_.push(srcNode, dstNode, w);    return true;}//template<typename N, typename E>//bool gdwg::Graph<N, E>::DeleteNode(const N& n) {////}
